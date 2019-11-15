3c651ba1821aaf1a7d31a76dbb447e70ab737934
Kaustubha DS <dskaustubh@gmail.com>
2019-11-15 20:44:20 +0530

    whistle podu

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask,render_template,request,session,redirect,jsonify,url_for,flash
 import pymysql.cursors
 import hashlib
-from keras.preprocessing import image
 from werkzeug.utils import secure_filename
 app =Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='',
-                             db='pulmacare',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor,
-                             autocommit=True)
+# connection = pymysql.connect(host='localhost',
+#                              user='root',
+#                              password='',
+#                              db='pulmacare',
+#                              charset='utf8mb4',
+#                              cursorclass=pymysql.cursors.DictCursor,
+#                              autocommit=True)
 
 @app.route("/")
 def index():
